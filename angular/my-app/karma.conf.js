5f0223a6d5522b01090b47eb7587aefc7e171e8a
Tomasz Belina <tomasz.belina@igt.com>
2018-03-03 18:22:47 +0100

    change test results file

--- angular/my-app/karma.conf.js
+++ angular/my-app/karma.conf.js
@@ -27,10 +27,10 @@ module.exports = function (config) {
     },
     reporters: ['progress', 'junit','coverage'],
     junitReporter: {
-	   outputDir: '', // results will be saved as $outputDir/$browserName.xml
-	   outputFile: 'test_jasmine_js.xml', // if included, results will be saved as $outputDir/$browserName/$outputFile
+	   outputDir: 'test_results', // results will be saved as $outputDir/$browserName.xml
+	   outputFile: 'result.xml', // if included, results will be saved as $outputDir/$browserName/$outputFile
 	   suite: '', // suite will become the package name attribute in xml testsuite element
-	   useBrowserName: true, // add browser name to report and classes names
+	   useBrowserName: false, // add browser name to report and classes names
 	   nameFormatter: undefined, // function (browser, result) to customize the name attribute in xml testcase element
 	   classNameFormatter: undefined, // function (browser, result) to customize the classname attribute in xml testcase element
 	   properties: {} // key value pair of properties to add to the section of the report
